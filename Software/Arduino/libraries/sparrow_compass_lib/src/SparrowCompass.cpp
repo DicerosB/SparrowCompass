c43893872ad747128a0503666bd21095768af200
DicerosB <Manuel.Petzoldt@gmx.de>
2025-12-21 01:53:36 +0100

    removing hardwaresetup.cpp

--- Software/Arduino/libraries/sparrow_compass_lib/src/SparrowCompass.cpp
+++ Software/Arduino/libraries/sparrow_compass_lib/src/SparrowCompass.cpp
@@ -1,39 +1,109 @@
 #include "SparrowCompass.h"
 
-String hello_world_message = F(
-  "\n~~~~~ Sparrow Compass ~~~~~~~\n "
-  "Arduino Library\n"
-  "Project Version: " PROJECT_VERSION "\n"
-  "Author: " PROJECT_AUTHOR
-  "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
-
-
-SparrowCompass::SparrowCompass(){
-  
+SparrowCompass::SparrowCompass(TwoWire* p_i2c, USBSerial* p_usb){
+  this->i2c = p_i2c;
+  this->usb = p_usb;
+  acc_module, gyr_module, mot_module, mag_module, gps_module = 0;
 }
 
 void SparrowCompass::begin(){
-  // general init
   hw_init();
   setup_usb();
-  Serial.println(hello_world_message);
+  usb->println(hello_world_message);
+  i2c->begin();
+  scan_for_modules();
+  init_modules();
+}
 
-  // init modules
+void SparrowCompass::init_modules(){
+
 }
 
 void SparrowCompass::work(){
-  // handle serial input
-  if(Serial.available()){
-    String buffer = Serial.readString();
+  // handle USB input
+  if(usb->available()){
+    String buffer = usb->readString();
     buffer.replace("\n", "");
     buffer.replace("\r", "");
     if(buffer == "deadbeef"){
-      Serial.println(F("switching to bootloader ..."));
       switch_to_bootloader();
+    }else{
+      usb->println(buffer);
     }
-    Serial.println(buffer);
   }
-  // handle motor
-  // handle sensors
-  // handle
+}
+
+void SparrowCompass::hw_init(){
+  pinMode(DEBUG_LED_Pin, OUTPUT);
+  pinMode(GPS_nReset_Pin, OUTPUT);
+  pinMode(MOT_nEnable_Pin, OUTPUT);
+  pinMode(MOT_STEP_Pin, OUTPUT);
+  pinMode(MOT_DIR_Pin, OUTPUT);
+
+  digitalWrite(GPS_nReset_Pin, HIGH);
+  digitalWrite(MOT_nEnable_Pin, HIGH);
+}
+
+void SparrowCompass::setup_usb(){
+  // force USB host to re-enumerate
+  pinMode(PA12, OUTPUT);
+  digitalWrite(PA12, LOW);
+  delay(100);
+  digitalWrite(PA12, HIGH);
+  HAL_GPIO_DeInit(GPIOA, GPIO_PIN_12);
+  usb->begin();
+}
+
+void SparrowCompass::scan_for_modules(){
+  uint8_t error, address;
+
+  usb->println("Scanning for Modules");
+
+  for(address = 1; address < 127; address++) {
+    i2c->beginTransmission(address);
+    error = i2c->endTransmission();
+
+    if (!error) {
+      switch(address){
+        case I2C_ADR_GYROSCOPE:
+          #ifdef ENABLE_GYROSCOPE
+            gyr_module = 1;
+            usb << "Gyroscope found.";
+          #endif
+        break;
+        case I2C_ADR_ACCELEROMETER:
+          #ifdef ENABLE_ACCELEROMETER
+            acc_module = 1;
+            usb->println("Accelerometer found.");
+          #endif
+        break;
+        case I2C_ADR_MAGNETOMETER:
+          #ifdef ENABLE_MAGNETOMETER
+            mag_module = 1;
+            usb->println("Magnetometer found.");
+          #endif
+        break;
+        case I2C_ADR_GPS:
+          #ifdef ENABLE_GPS
+            gps_module = 1;
+            usb->println("GPS found.");
+          #endif
+        break;
+        default:
+          usb->println("Unknown module found at address 0x%2H", address);
+        break;
+      }
+    }
+    else if (error == 4) {
+      usb->println("Communication error at address 0x%2H", address);
+    }
+  }
+}
+
+void SparrowCompass::switch_to_bootloader(){
+  usb->println(F("switching to bootloader ..."));
+  usb->flush();
+	uint32_t* dfu_boot_flag = (uint32_t*)(&_bflag);
+	*dfu_boot_flag = DFU_BOOT_FLAG;
+	HAL_NVIC_SystemReset();
 }
--- Software/Arduino/libraries/sparrow_compass_lib/src/SparrowCompass.h
+++ Software/Arduino/libraries/sparrow_compass_lib/src/SparrowCompass.h
@@ -2,10 +2,51 @@
 #define _SPARROWCOMPASS_H_
 
 #include <Arduino.h>
-#include "hardware_setup.h"
+#include <Wire.h>
 
+// boot flag value for entering bootloader mode
+#define DFU_BOOT_FLAG 0xDEADBEEF
+
+// enabled device modules
+#define ENABLE_MOTOR
+#define ENABLE_GYROSCOPE
+#define ENABLE_MAGNETOMETER
+#define ENABLE_ACCELEROMETER
+#define ENABLE_GPS
+
+// I2C addresses of specific modules
+#define I2C_ADR_GYROSCOPE 0x6B
+#define I2C_ADR_MAGNETOMETER 0x1E
+#define I2C_ADR_ACCELEROMETER 0x19
+#define I2C_ADR_GPS 0x41
+
+// hardware pinout
+#define DEBUG_LED_Pin PC13
+#define GPS_nReset_Pin PA0
+#define GPS_INT_Pin PA1
+#define MAG_DRDY_Pin PA2
+#define MAG_INT1_Pin PA3
+#define GYR_DRDY_Pin PA4
+#define MOT_nEnable_Pin PB11
+#define MOT_STEP_Pin PB12
+#define MOT_DIR_Pin PB13
+#define MOT_SENS_B_Pin PB14
+#define MOT_SENS_A_Pin PB15
+#define MOT_SENS_B_EXTI_IRQn EXTI15_10_IRQn
+#define MOT_SENS_A_EXTI_IRQn EXTI15_10_IRQn
+
+// project metadata
 #define PROJECT_VERSION "0.0.1"
 #define PROJECT_AUTHOR "Petzoldt"
+
+extern int _bflag;
+String hello_world_message = F(
+  "\n~~~~~ Sparrow Compass ~~~~~~~\n "
+  "Arduino Library\n"
+  "Project Version: " PROJECT_VERSION "\n"
+  "Author: " PROJECT_AUTHOR
+  "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+  
 
 #ifdef __cplusplus
 extern "C" {
@@ -13,11 +54,21 @@ extern "C" {
 
 class SparrowCompass{
   public:
-  USBSerial *usb;
-  SparrowCompass();
+  
+  SparrowCompass(TwoWire* p_i2c, USBSerial* p_usb);
   void begin();
   void work();
+
   private:
+  bool acc_module, gyr_module, mot_module, mag_module, gps_module;
+  TwoWire *i2c;
+  USBSerial *usb;
+
+  void switch_to_bootloader();
+  void scan_for_modules();
+  void setup_usb();
+  void hw_init();
+  void init_modules();
 };
 
 #ifdef __cplusplus
